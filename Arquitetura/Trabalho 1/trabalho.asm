337a1b86227f4ded3f6eeadb0ba72f695eeab50e
Marcus Neto <163374075+mnetoo@users.noreply.github.com>
2025-04-08 18:01:15 -0300

    trabalho finalizado

--- Arquitetura/Trabalho 1/trabalho.asm
+++ Arquitetura/Trabalho 1/trabalho.asm
@@ -63,52 +63,65 @@ VETOR_A:
 
 FIM_VETOR_A:
     sub r0, r0
-    ;   CARREGAR O FINAL DO VETOR R
+
+    ;   CARREGANDO O INICIO DE R
     addi 7
     slr r0, r0
     sub r3, r3
     add r3, r0
     sub r0, r0
-    addi 7
-    addi 2
-    add r3, r0  ; r3 = 137
+    ;addi 1
+    add r3, r0  ; r3 = 129
 
     sub r0, r0
     sub r1, r1
     sub r2, r2
 
-    ;   CARREGANDO O ULTIMO VALOR DO VETOR R
+    ;   CARREGANDO O DO INICIO DE A
     addi 7
-    addi 7
-    addi 7
-    addi 7
-    addi 7
-    addi 2
-    add r2, r0  ; r2 = 37
-
-    ;   CARREGANDO ENDEREÇO DE SAÍDA DO LOOP
-    addi 7
-    addi 7
-    addi 7
-    addi 7
-    addi 7
-    add r1, r0  ; r1 = 61
-
+    slr r0, r0
+    add r1, r0
     sub r0, r0
-    addi 1
+    addi 7
+    addi 3
+    sub r1, r0  ; r1 = 118
+    sub r1, r0  ; r1 = 108
 
 VETOR_SOMA:
-    st r2, r3   ; M[r3] = r2
-    sub r2, r0  ; r2 - 1
-    brzr r2, r1     ; if r2 = 0, goto r1 = fim do loop
-    sub r2, r0  ; r2 - 1
-    sub r2, r0  ; r2 - 1
-    sub r2, r0  ; r2 - 1
-    sub r3, r0  ; r3 -1
-    ji VETOR_SOMA
-
-FIM_SOMA:
+    ;   r0 = 10
     sub r0, r0
-    sub r1, r1
-    sub r2, r2
-    sub r3, r3
+    addi 7
+    addi 3
+
+    ld r2, r1   ; A[i] - conteudo de A
+    add r0, r1  ; add(B) - endereço de b
+    ld r0, r0   ; B[i] - conteudo de B
+    add r2, r0  ; A[i] + B[i] - soma
+    st r2, r3   ; R[i] = soma
+
+    sub r0, r0
+
+    ;   ENDEREÇO +1
+    addi 1
+    add r1, r0
+    add r3, r0
+
+    ;   CARREGA PARA FAZER A VERIFICAÇÃO
+    sub r0, r0
+    addi 7
+    addi 7
+    addi 7
+    addi 7
+    addi 7
+    addi 2  ; r0 = 37
+    
+    ;   FAZ A VERIFICAÇÃO DO BRZR
+    sub r2, r0
+    not r2, r2
+    addi 3  
+    addi 7
+    addi 3;
+    addi 5  ;r0 = 47 endereço para voltar
+    brzr r2, r0
+    
+ebreak
