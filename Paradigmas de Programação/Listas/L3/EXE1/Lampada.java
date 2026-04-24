c33be668c3df914d99569c172ba70cbe27a82e27
Marcus Neto <163374075+mnetoo@users.noreply.github.com>
2026-04-24 10:39:01 -0300

    Refactor Lampada methods and rename isAtivo to isEstado

--- Paradigmas de Programação/Listas/L3/EXE1/Lampada.java
+++ Paradigmas de Programação/Listas/L3/EXE1/Lampada.java
@@ -7,13 +7,14 @@ public class Lampada
     public Lampada(boolean estado) { setEstado(estado); }
 
     // Getters
-    public boolean isAtivo() { return this.estado; }
+    public boolean isEstado() { return this.estado; }
     
     // Setters
     public void setEstado(boolean estado) { this.estado = estado; }
     
     // Métodos
-    public void alterar() { this.estado = !this.estado; }
+    public void ligar() { this.estado = true; }
+    public void desligar()  { this.estado = false; }
     
     @Override
     public String toString() { return this.estado ? "Ligada" : "Desligada"; }
